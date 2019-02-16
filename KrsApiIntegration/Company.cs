c4399acd37fb8d8618b9c9a13e49af9b0bacf5b4
ziombo <jasiu@360.st>
2019-02-16 02:59:27 +0100

    Add project files.

---
+++ KrsApiIntegration/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace KrsApiIntegration
+{
+    public class Company
+    {
+        private string name;
+        private string krs;
+
+        public string Krs { get => krs; set => krs = value; }
+        public string Name { get => name; set => name = value; }
+
+    }
+}
